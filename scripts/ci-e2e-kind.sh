46523dc2ce9813172c06ca863230c3b9d1112b31
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2021-05-24 11:10:24 -0700

    feat: adds upgrade tests

--- scripts/ci-e2e-kind.sh
+++ scripts/ci-e2e-kind.sh
@@ -5,46 +5,8 @@ set -o nounset
 set -o pipefail
 
 create_kind_cluster () {
-  # create a cluster with the local registry enabled in containerd
-  # add encryption config and the kms static pod manifest with custom image
-  cat <<EOF | kind create cluster --retain --image kindest/node:"${KUBERNETES_VERSION}" --name "${KIND_CLUSTER_NAME}" --wait 2m --config=-
-  kind: Cluster
-  apiVersion: kind.x-k8s.io/v1alpha4
-  containerdConfigPatches:
-  - |-
-    [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-      endpoint = ["http://${reg_host}:${reg_port}"]
-  nodes:
-  - role: control-plane
-    extraMounts:
-    - containerPath: /etc/kubernetes/encryption-config.yaml
-      hostPath: tests/e2e/encryption-config.yaml
-      readOnly: true
-      propagation: None
-    - containerPath: /etc/kubernetes/manifests/kubernetes-kms.yaml
-      hostPath: tests/e2e/generated_manifests/kms.yaml
-      readOnly: true
-      propagation: None
-    - containerPath: /etc/kubernetes/azure.json
-      hostPath: tests/e2e/generated_manifests/azure.json
-      readOnly: true
-      propagation: None
-    kubeadmConfigPatches:
-      - |
-        kind: ClusterConfiguration
-        apiServer:
-          extraArgs:
-            encryption-provider-config: "/etc/kubernetes/encryption-config.yaml"
-          extraVolumes:
-          - name: encryption-config
-            hostPath: "/etc/kubernetes/encryption-config.yaml"
-            mountPath: "/etc/kubernetes/encryption-config.yaml"
-            readOnly: true
-            pathType: File
-          - name: sock-path
-            hostPath: "/opt"
-            mountPath: "/opt"
-EOF
+  ls -latr
+  . scripts/kind-cluster.sh
 }
 
 connect_registry () {
