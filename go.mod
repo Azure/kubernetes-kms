b245b071f0b30ec16d6225cf9fa9949a067ba45a
Anish Ramasekar <anish.ramasekar@gmail.com>
2023-01-04 07:38:50 +0000

    feat: migrate from `autorest/adal` to `azidentity`
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/Azure/kubernetes-kms
 go 1.19
 
 require (
-	github.com/Azure/azure-sdk-for-go v67.2.0+incompatible
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys v0.9.0
 	github.com/Azure/go-autorest/autorest v0.11.28
-	github.com/Azure/go-autorest/autorest/adal v0.9.21
 	go.opentelemetry.io/otel v0.20.0
 	go.opentelemetry.io/otel/exporters/metric/prometheus v0.20.0
 	go.opentelemetry.io/otel/metric v0.20.0
@@ -19,21 +20,26 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/keyvault/internal v0.7.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.21 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/zapr v1.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
--- pkg/plugin/keyvault.go
+++ pkg/plugin/keyvault.go
@@ -7,8 +7,8 @@ package plugin
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
 
@@ -18,8 +18,8 @@ import (
 	"github.com/Azure/kubernetes-kms/pkg/utils"
 	"github.com/Azure/kubernetes-kms/pkg/version"
 
-	kv "github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
-	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"k8s.io/klog/v2"
 )
@@ -31,13 +31,13 @@ type Client interface {
 }
 
 type keyVaultClient struct {
-	baseClient       kv.BaseClient
+	keysClient       *azkeys.Client
 	config           *config.AzureConfig
 	vaultName        string
 	keyName          string
 	keyVersion       string
 	vaultURL         string
-	azureEnvironment *azure.Environment
+	azureEnvironment azure.Environment
 }
 
 // NewKeyVaultClient returns a new key vault client to use for kms operations
@@ -58,12 +58,8 @@ func newKeyVaultClient(
 	if len(vaultName) == 0 || len(keyName) == 0 || len(keyVersion) == 0 {
 		return nil, fmt.Errorf("key vault name, key name and key version are required")
 	}
-	kvClient := kv.New()
-	err := kvClient.AddToUserAgent(version.GetUserAgent())
-	if err != nil {
-		return nil, fmt.Errorf("failed to add user agent to keyvault client, error: %+v", err)
-	}
-	env, err := auth.ParseAzureEnvironment(config.Cloud)
+
+	env, err := parseAzureEnvironment(config.Cloud)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse cloud environment: %s, error: %+v", config.Cloud, err)
 	}
@@ -75,70 +71,76 @@ func newKeyVaultClient(
 	if vaultResourceURL == azure.NotAvailable {
 		return nil, fmt.Errorf("keyvault resource identifier not available for cloud: %s", env.Name)
 	}
-	token, err := auth.GetKeyvaultToken(config, env, vaultResourceURL, proxyMode)
+	cred, err := auth.GetTokenCredential(config, env.ActiveDirectoryEndpoint, vaultResourceURL, proxyMode)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key vault token, error: %+v", err)
 	}
-	kvClient.Authorizer = token
 
 	vaultURL, err := getVaultURL(vaultName, managedHSM, env)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get vault url, error: %+v", err)
 	}
 
+	t := &transporter{}
+	t.AddDecorator(SetUserAgent)
+
 	if proxyMode {
-		kvClient.RequestInspector = autorest.WithHeader(consts.RequestHeaderTargetType, consts.TargetTypeKeyVault)
 		vaultURL = getProxiedVaultURL(vaultURL, proxyAddress, proxyPort)
+		t.AddDecorator(SetProxyHeader)
 	}
 
 	klog.InfoS("using kms key for encrypt/decrypt", "vaultURL", *vaultURL, "keyName", keyName, "keyVersion", keyVersion)
 
-	client := &keyVaultClient{
-		baseClient:       kvClient,
+	opts := &azkeys.ClientOptions{
+		ClientOptions: azcore.ClientOptions{
+			Transport: t,
+		},
+	}
+	keysClient, err := azkeys.NewClient(*vaultURL, cred, opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create keyvault client, error: %+v", err)
+	}
+
+	return &keyVaultClient{
+		keysClient:       keysClient,
 		config:           config,
 		vaultName:        vaultName,
 		keyName:          keyName,
 		keyVersion:       keyVersion,
 		vaultURL:         *vaultURL,
 		azureEnvironment: env,
-	}
-	return client, nil
+	}, nil
 }
 
-func (kvc *keyVaultClient) Encrypt(ctx context.Context, cipher []byte) ([]byte, error) {
-	value := base64.RawURLEncoding.EncodeToString(cipher)
-
-	params := kv.KeyOperationsParameters{
-		Algorithm: kv.RSA15,
-		Value:     &value,
+func (c *keyVaultClient) Encrypt(ctx context.Context, cipher []byte) ([]byte, error) {
+	jsonWebKeyEncryptionAlgorithmRSA15 := azkeys.JSONWebKeyEncryptionAlgorithmRSA15
+	params := azkeys.KeyOperationsParameters{
+		Algorithm: &jsonWebKeyEncryptionAlgorithmRSA15,
+		Value:     cipher,
 	}
-	result, err := kvc.baseClient.Encrypt(ctx, kvc.vaultURL, kvc.keyName, kvc.keyVersion, params)
+
+	response, err := c.keysClient.Encrypt(ctx, c.keyName, c.keyVersion, params, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to encrypt, error: %+v", err)
 	}
-	return []byte(*result.Result), nil
+	return response.Result, nil
 }
 
-func (kvc *keyVaultClient) Decrypt(ctx context.Context, plain []byte) ([]byte, error) {
-	value := string(plain)
-
-	params := kv.KeyOperationsParameters{
-		Algorithm: kv.RSA15,
-		Value:     &value,
+func (c *keyVaultClient) Decrypt(ctx context.Context, plain []byte) ([]byte, error) {
+	jsonWebKeyEncryptionAlgorithmRSA15 := azkeys.JSONWebKeyEncryptionAlgorithmRSA15
+	params := azkeys.KeyOperationsParameters{
+		Algorithm: &jsonWebKeyEncryptionAlgorithmRSA15,
+		Value:     plain,
 	}
 
-	result, err := kvc.baseClient.Decrypt(ctx, kvc.vaultURL, kvc.keyName, kvc.keyVersion, params)
+	response, err := c.keysClient.Decrypt(ctx, c.keyName, c.keyVersion, params, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decrypt, error: %+v", err)
 	}
-	bytes, err := base64.RawURLEncoding.DecodeString(*result.Result)
-	if err != nil {
-		return nil, fmt.Errorf("failed to base64 decode result, error: %+v", err)
-	}
-	return bytes, nil
+	return response.Result, nil
 }
 
-func getVaultURL(vaultName string, managedHSM bool, env *azure.Environment) (vaultURL *string, err error) {
+func getVaultURL(vaultName string, managedHSM bool, env azure.Environment) (vaultURL *string, err error) {
 	// Key Vault name must be a 3-24 character string
 	if len(vaultName) < 3 || len(vaultName) > 24 {
 		return nil, fmt.Errorf("invalid vault name: %q, must be between 3 and 24 chars", vaultName)
@@ -164,16 +166,47 @@ func getProxiedVaultURL(vaultURL *string, proxyAddress string, proxyPort int) *s
 	return &proxiedVaultURL
 }
 
-func getVaultDNSSuffix(managedHSM bool, env *azure.Environment) string {
+func getVaultDNSSuffix(managedHSM bool, env azure.Environment) string {
 	if managedHSM {
 		return env.ManagedHSMDNSSuffix
 	}
 	return env.KeyVaultDNSSuffix
 }
 
-func getVaultResourceIdentifier(managedHSM bool, env *azure.Environment) string {
+func getVaultResourceIdentifier(managedHSM bool, env azure.Environment) string {
 	if managedHSM {
 		return env.ResourceIdentifiers.ManagedHSM
 	}
 	return env.ResourceIdentifiers.KeyVault
+}
+
+// parseAzureEnvironment returns azure environment by name
+func parseAzureEnvironment(cloudName string) (azure.Environment, error) {
+	if cloudName == "" {
+		return azure.PublicCloud, nil
+	}
+	return azure.EnvironmentFromName(cloudName)
+}
+
+type transporter struct {
+	decorators []func(*http.Request)
+}
+
+func (t *transporter) AddDecorator(decorator func(*http.Request)) {
+	t.decorators = append(t.decorators, decorator)
+}
+
+func SetUserAgent(req *http.Request) {
+	req.Header.Set("User-Agent", version.GetUserAgent())
+}
+
+func SetProxyHeader(req *http.Request) {
+	req.Header.Set(consts.RequestHeaderTargetType, consts.TargetTypeKeyVault)
+}
+
+func (t *transporter) Do(req *http.Request) (*http.Response, error) {
+	for _, decorator := range t.decorators {
+		decorator(req)
+	}
+	return http.DefaultTransport.RoundTrip(req)
 }
